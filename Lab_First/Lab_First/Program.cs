f5393cd639ae06fe4b1b0a780a6833dca7506c5d
makatov <samakat1999@gmail.com>
2018-01-19 10:24:46 +0600

    First Task

---
+++ Lab_First/Lab_First/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string line = Console.ReadLine();//создаем тип строки
+			string[] arr = line.Split(' ');// создаем и записываем массив через пробел
+			for (int i = 0; i < arr.Length; i++)
+			{
+				int n = int.Parse(arr[i]);// парсим из строки в тип integer
+				bool checker = true;// создаем boolean тип
+				if (n == 1)
+				{
+					continue;
+				}
+				for (int j = 2; j <= Math.Sqrt(n); j++)
+				{
+					if (n % j == 0)
+					{
+						checker = false;// проверка на простое число
+						break;
+					}
+				}
+				if (checker == true)
+				{
+					Console.WriteLine(n);// вывод данных
+				}
+			}
+			Console.ReadKey();//ожидание клавиши 
+		}
+	}
+}
