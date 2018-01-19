6140fd5620095a9ad1a6ade4dbfee0c464467ebe
makatov <samakat1999@gmail.com>
2018-01-19 11:11:08 +0600

    The last update

---
+++ Lab_First_4_Task/Lab_First_4_Task/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Lab_First_4_Task
+{
+    public class Circle
+    {
+
+		public double radius;
+		public double area;
+
+		public Circle()
+		{
+			radius = 0;
+		}
+
+		public Circle(double radius)
+		{
+			this.radius = radius;
+			findArea();
+		}
+
+		public void findArea()
+		{
+			area = Math.PI * radius * radius;
+		}
+
+		public double findArea2()
+		{
+			return Math.PI * radius * radius;
+		}
+
+		public override string ToString()
+		{
+			return "radius = " + radius + "\narea = " + area;
+		}
+
+
+	
+    }
+}
---
+++ Lab_First_4_Task/Lab_First_4_Task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_First_4_Task
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+
+        {
+            double rd = double.Parse(Console.ReadLine());
+
+            Circle c = new Circle(rd);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
