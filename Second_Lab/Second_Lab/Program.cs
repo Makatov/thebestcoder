02aeee593acb1e0238b1866eb9792c0e6513a53e
makatov <samakat1999@gmail.com>
2018-02-06 20:51:50 +0600

    Second Lab

---
+++ Second_Lab/Second_Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Second_Lab
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
