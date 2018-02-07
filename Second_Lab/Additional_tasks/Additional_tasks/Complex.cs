a7600b184a14f3cba6b579d00fdd90cfa47a4aa5
makatov <samakat1999@gmail.com>
2018-02-07 12:41:11 +0600

    third pravka

---
+++ Second_Lab/Additional_tasks/Additional_tasks/Complex.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace Additional_tasks
+{
+	class Complex
+	{
+		public int a;
+		public int b;
+
+		public Complex(int _a, int _b)
+		{
+			a = _a;
+			b = _b;
+		}
+		public static int gcd(int n, int m)
+		{
+			while (m != n)
+			{
+				if (m > n)
+					m = m - n;
+				else
+					n = n - m;
+			}
+			return m;
+		}
+		public static int lcm(int n, int m)
+		{
+			return m * n / gcd(m, n);
+		}
+		public override string ToString()
+		{
+			return a + "/" + b;
+		}
+		public static Complex operator +(Complex c1, Complex c2)
+		{
+			int side = lcm(c1.b, c2.b);
+			Complex c = new Complex(c1.a * (side / c2.b) + c2.a * (side / c1.b), side);
+			c.Simplify();
+			return c;
+		}
+		public static Complex operator -(Complex c1, Complex c2)
+		{
+			int side = lcm(c1.b, c2.b);
+
+			Complex c = new Complex(c1.a * c2.b - c2.a * c1.b, side);
+			c.Simplify();
+			return c;
+		}
+		public static Complex operator *(Complex c1, Complex c2)
+		{
+
+			Complex c = new Complex(c1.a * c2.a, c1.b * c2.b);
+			c.Simplify();
+			return c;
+		}
+		public static Complex operator /(Complex c1, Complex c2)
+		{ 
+
+			Complex c = new Complex(c1.a * c2.b, c1.b * c2.a);
+			c.Simplify();
+			return c;
+		}
+
+		public void Simplify()
+		{
+			int _a = this.a;
+			int _b = this.b;
+			while (_a > 0 && _b > 0)
+			{
+				if (_a > _b)
+					_a = _a % _b;
+				else
+					_b = _b % _a;
+			}
+			int nod = _a + _b;
+			a /= nod;
+			b /= nod;
+		}
+	}
+    }
---
+++ Second_Lab/Additional_tasks/Additional_tasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Additional_tasks
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
