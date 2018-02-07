d586746dcaaa1c7f7213b50573b8b8015e08c33e
makatov <samakat1999@gmail.com>
2018-02-07 12:26:31 +0600

    The second pravka

---
+++ Second_Lab/PrimefromFile/PrimefromFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrimefromFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string ans = "";
+			string text = System.IO.File.ReadAllText(@"/Users/samat/Desktop/Programing_P2/Second_Lab/PrimeIn.txt");
+
+			string[] arr = text.Split(' ');
+			int sum = 0;
+			for (int i = 0; i < arr.Length; i++)
+			{
+				sum = int.Parse(arr[i]);
+				if (sum == 1) continue;
+				else
+				{
+					int j = 0;
+					int cnt = 0;
+					for (j = 2; j <= Math.Sqrt(sum); j++)
+					{
+						if (sum % j == 0)
+						{
+                            cnt = 1;
+							break;
+						}
+					}
+					if (cnt == 0)
+						ans = ans + arr[i] + " ";
+					cnt = 0;
+				}
+			}
+			System.IO.File.WriteAllText(@"/Users/samat/Desktop/Programing_P2/Second_Lab/PrimeOut.txt", ans);
+		}
+	}
+}
---
+++ Second_Lab/minmax/minmax/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadFromFileMaxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = "";
+            string text = System.IO.File.ReadAllText(@"/Users/samat/Desktop/Programing_P2/Second_Lab/INforMaxandMin.txt");
+
+            string[] arr = text.Split(' ');
+            int min = int.Parse(arr[0]);
+            int max = int.Parse(arr[0]);
+            int sum = 0;
+            string mn = "", mx = "";
+            for(int i = 0;i<=(arr.Length)-1;i++)
+            {
+                sum = int.Parse(arr[i]);
+                if (sum <= min)
+                {
+                    min = sum;
+                    mn = arr[i];
+                }
+                if (sum >= max)
+                {
+                    max = sum;
+                    mx = arr[i];
+                }
+                
+            }
+            ans = mn + " " + mx;
+            System.IO.File.WriteAllText(@"/Users/samat/Desktop/Programing_P2/Second_Lab/OUTforMaxandMin.txt", ans);
+        }
+    }
+}
