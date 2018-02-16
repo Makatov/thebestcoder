62aed6f9a4943fdece13feca6a2cd6612dbe928c
Makatov <35269333+Makatov@users.noreply.github.com>
2018-02-16 10:22:19 +0600

    Update Program.cs

--- Second_Lab/Additional_tasks/Additional_tasks/Program.cs
+++ Second_Lab/Additional_tasks/Additional_tasks/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Additional_tasks
+namespace OnLab
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line = Console.ReadLine();
+            
+            string[] s = line.Split(' ');
+            string[] n1 = s[0].Split('/');
+            string[] n2 = s[1].Split('/');
+            int a = int.Parse(n1[0]);
+            int b = int.Parse(n1[1]);
+            int c = int.Parse(n2[0]);
+            int d = int.Parse(n2[1]);
+
+            Complex c1 = new Complex(a, b);
+            Complex c2 = new Complex(c, d);
+
+            c1.Simplify();
+            c2.Simplify();
+            
+
+            Complex sum = c1 + c2;
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
         }
     }
 }
